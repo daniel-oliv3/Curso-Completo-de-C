ac4364a7026eff7d3354ae488634ecf48a22c4be
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-05 15:37:53 -0300

    11 - Diretivas de compilação

---
+++ 11 - Diretivas de compilação/134 - include.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+/* 134 - include;*/
+
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	system("color 1f");
+	
+	
+	system("Pause");
+	return 0;
+}
+
+/**/
